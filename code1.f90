0415d751b426f849592e14687ead7427ae27ec0a
root <root@localhost.localdomain>
2014-08-17 02:50:05 -0700

    3x3 matrix used

--- code1.f90
+++ code1.f90
@@ -2,8 +2,8 @@ program code1
 	use mathlib
 	implicit none
 	real :: val1
-	real, dimension(4, 4) :: b, val2
-	b = reshape((/1, 2, 3, 4, 5, 6, 1 ,2 ,2,1,2,3,4,5,6,7/),shape(b))
+	real, dimension(3, 3) :: b, val2
+	b = reshape((/1, 2, 3, 4, 5, 6, 1 ,2 ,2/),shape(b))
 	val1 = det(b)
 	val2 = inv(b)
 	print*,'Determinant of the matrix is : ', val1
