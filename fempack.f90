fc07e3947d338d616bf814791f80540be93c6619
ravishverma <ravish7v@gmail.com>
2014-12-15 02:37:30 -0800

    MESSAGE: verification added

---
+++ fempack.f90
@@ -0,0 +1,18 @@
+module fempack
+!===============================================================================
+! Necessary mathematical functions
+!
+! List:
+! 1. Mesh a 2D geometry [subroutine]
+! Info:
+!		Syntax: call mesh2Dquad()
+!		Result: The meshing information is stored in matrices
+!               nodes[nx2], elements[nex4]
+!		Description: Paving algorithm
+!		Caution: 
+!===============================================================================
+	implicit none
+contains
+	subroutine mesh2Dquad()
+
+	end subroutine mesh2Dquad
